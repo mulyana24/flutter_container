9796a9318ec7cb3902c9b19ca74aec373aad1ef5
mulyana24 <49643139+mulyana24@users.noreply.github.com>
2021-03-30 19:10:27 +0700

    first update

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(25, 25, 25, 25),
+          padding: EdgeInsets.only(
+            bottom: 20,
+            top: 20,
+          ),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.amber, Colors.blue])),
+          ),
+        ),
+      ),
+    );
+  }
+}
